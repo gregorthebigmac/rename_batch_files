1cc0f2a36a814d38a6b875d9bd13629650ecb6b4
gregorthebigmac <gregorthebigmac@gmail.com>
2018-12-30 12:00:57 -0600

    done for now. coming back to it later

--- batch_rename.cpp
+++ batch_rename.cpp
@@ -25,5 +25,38 @@ int main(int argc, char *argv[]) {
 	for (int i = 2; i < file_list.size(); i++) { // starting at 2 because 0 and 1 are just "." and ".."
 		cout << file_list[i] << endl;
 	}
+	bool done = false;
+	while (!done) {
+		cout << "Are these the files you want to perform the batch command upon? [y/n]" << endl;
+		string u_answer;
+		std::getline(std::cin, u_answer);
+		if (u_answer[0] == 'y' || u_answer[0] == 'Y')
+			done = true;
+		else
+			return 0;
+	}
+	vector<string> terminal_feedback, error_list;
+	vector<string> command_queue;
+	/*
+		TODO: Ask use what command(s) they want to execute on each file
+		Give the user the option to include the file by typing "file_name"
+		e.g. "what command(s) would you like to execute on each file? use
+		"file_name" to indicate you want the name of the file inserted here."
+		$ cp
+		"add another? [y/n]"
+		$ y
+		"what command(s) would you like to execute on each file? use "file_name"
+		to indicate you want the name of the file inserted here."
+		$ file_name
+		"add another? [y/n]"
+		$ y
+		"what command(s) would you like to execute on each file? use "file_name"
+		to indicate you want the name of the file inserted here."
+		$ 
+	*/
+	cout << "This is a test. I'm displaying the commands as they would be output to the terminal:" << endl;
+	for (int i = 2; i < file_list.size(); i++) {
+		// TODO: combine commands and files and push_back command_queue
+	}
 	return 0;
 }
