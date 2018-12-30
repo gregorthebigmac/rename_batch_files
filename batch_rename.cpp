d063734ab3aa65cd68d14adf2e4e77ab7a669366
gregorthebigmac <gregorthebigmac@gmail.com>
2018-12-30 11:18:08 -0600

    got list sorted and ignoring first two "files"

--- batch_rename.cpp
+++ batch_rename.cpp
@@ -1,20 +1,29 @@
 #include <dirent.h>
+#include <algorithm>
 #include "command/command.hpp"
 
 using std::cout;
 using std::endl;
-
+using std::vector;
+using std::string;
 command cmd;
 
 int main(int argc, char *argv[]) {
+	vector<string> file_list;
 	DIR *d;
 	struct dirent *dir;
 	d = opendir(".");
 	if (d) {
 		while ((dir = readdir(d)) != NULL) {
-			cout << dir->d_name << endl;
+			file_list.push_back(dir->d_name);
+			cout << "Adding " << dir->d_name << " to list..." << endl;
 		}
 		closedir(d);
+	}
+	std::sort(file_list.begin(), file_list.end());
+	cout << "printing file list:" << endl;
+	for (int i = 2; i < file_list.size(); i++) { // starting at 2 because 0 and 1 are just "." and ".."
+		cout << file_list[i] << endl;
 	}
 	return 0;
 }
